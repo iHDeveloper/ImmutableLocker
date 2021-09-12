dba977c4b723095b41adf023523926ade77416d4
iHDeveloper <git@ihdeveloper.me>
2021-09-13 01:20:31 +0300

    Prepare before implemeting the locker generator
    I installed all the necessary libraries for handling cryptographic
    operations. And, changed some assets because they were not related.
    This commit is going to be used as a fallback in case I messed up
    writing the implementation of this locker generator.

---
+++ public/js/build.js
@@ -0,0 +1,23 @@
+function hashPassphrase(passphrase) {
+    // TODO hash the passphrase using SHA-256
+}
+
+function checksum(data) {
+    // TODO checksum the data using SHA-512
+}
+
+function generateSalt() {
+    // TODO generate a 64bit random salt using crypto API
+}
+
+function generateKey(hash, salt) {
+    // TODO encrypt the hash with salt using scrypt
+}
+
+function ecnrypt(key, data) {
+    // TODO encode the data within private key using AES-256
+}
+
+function encode(encryptedData) {
+    // TODO encode encrypted data to Base64
+}
