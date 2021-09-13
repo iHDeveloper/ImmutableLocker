44a94b558e5c062a79633b7e4575db3492b4befc
iHDeveloper <git@ihdeveloper.me>
2021-09-13 12:53:25 +0300

    Implement the first builder for immutable lockers! \o/
    This is the first version (aka v1) for immutable locker protocol.
    This protocol currently supports encrypting messages for now only.
    
    The metadata is unencrypted and open to everyone.
    Note: I'm thinking of providing two-way encryption for the metadata.
    Because, it doesn't have to be public to everyone.
    
    This version provides a strong way of generating an encryption key
    from a passphrase no matter how weak it is.
    
    Implementation:
    
    The passphrase is being hashed with SHA-256.
    (Note: The passphrase is normalized with `NFKC` and encoded to bytes)
    Then, we generate a 64-bit of salt (CSPRN). Then, we generate a
    password-based key derivation using "scrypt" to provide the locker
    the ability for the locker key to be encrypted forever.
    (SCRYPT PARAMS: N=2^16, R=16, P=1)
    
    After generating the encryption key we can use this key to
    encrypt the locker data with AES-256.
    
    Important Note: The locker only stores the salt for uniqueness.
    
    the file with ".locker.checksum" is used to verify the given locker!

---
+++ example/README.md
@@ -0,0 +1,6 @@
+# Immutable Lockers Prototype
+This directory includes the first prototype of immutable lockers. Each protocol version has its own locker prototype.
+
+
+Currently, there's only the `v1` locker prototype.
+
--- public/js/build.js
+++ public/js/build.js
@@ -1,23 +1,118 @@
-function hashPassphrase(passphrase) {
-    // TODO hash the passphrase using SHA-256
+import { bytesToBase64, base64encode } from '/js/lib/base64.js';
+
+async function hashPassphrase(passphrase) {
+    // Hash the passphrase using SHA-256
+    const encoded = new TextEncoder().encode(passphrase.normalize("NFKC"));
+    const hash = await crypto.subtle.digest('SHA-256', encoded);
+    return new Uint8Array(hash);
 }
 
-function checksum(data) {
-    // TODO checksum the data using SHA-512
+async function checksum(data) {
+    // Checksum the data using SHA-512
+    const encoded = new TextEncoder().encode(data.normalize("NFKC"));
+    const hash = await crypto.subtle.digest('SHA-512', encoded);
+    return new Uint8Array(hash);
 }
 
 function generateSalt() {
-    // TODO generate a 64bit random salt using crypto API
+    // Generate a 64bit random salt using crypto API
+    const array = new Uint8Array(8);
+    crypto.getRandomValues(array);
+    return array;
 }
 
-function generateKey(hash, salt) {
-    // TODO encrypt the hash with salt using scrypt
+async function generateKey(hash, salt) {
+    // Encrypt the hash with salt using scrypt
+    return await scrypt.scrypt(hash, salt, 16, 16, 1, 32);
 }
 
-function ecnrypt(key, data) {
-    // TODO encode the data within private key using AES-256
+function encrypt(key, content) {
+    // Encode the data within private key using AES-256
+    const data = new TextEncoder().encode(content.normalize("NFKC"));
+    const aes = new aesjs.ModeOfOperation.ctr(key);
+    return aes.encrypt(data);
 }
 
 function encode(encryptedData) {
-    // TODO encode encrypted data to Base64
+    // Encode encrypted data to Base64
+    return bytesToBase64(encryptedData);
 }
+
+function bufferToHex(buffer) {
+    return Array.from(buffer).map(x => x.toString(16).padStart(2, '0')).join('');
+}
+
+async function onBuild(metadata, content, passphrase, hint) {
+    // Generate locker key
+    const hash = await hashPassphrase(passphrase);
+    const salt = generateSalt();
+    const lockerKey = await generateKey(hash, salt);
+    const encryptedData = encrypt(lockerKey, content);
+    const encodedData = encode(encryptedData);
+    document.getElementById("encrypted-content-preview").innerText = encodedData;
+
+    var locker = {
+        version: 'v1',
+        metadata,
+        security: {
+            salt: bytesToBase64(salt),
+            hint: base64encode(hint)
+        },
+        content: encodedData
+    };
+
+    const nonEncodedLocker = JSON.stringify(locker);
+    const encodedLocker = base64encode(nonEncodedLocker);
+    const lockerChecksum = await checksum(encodedLocker);
+    document.getElementById("non-encoded-locker-preview").innerText = nonEncodedLocker;
+    document.getElementById("encoded-locker-preview").innerText = encodedLocker;
+    document.getElementById("checksum-preview").innerText = bufferToHex(lockerChecksum);
+}
+
+document.getElementById("build-form").onsubmit = (e) => {
+    e.preventDefault();
+
+    // Metadata
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const description = document.getElementById("description").value;
+
+    // Content
+    const content = document.getElementById("content").value;
+
+    // Security
+    const passphrase = document.getElementById("passphrase").value;
+    const passphraseHint = document.getElementById("passphrase-hint").value;
+
+    onBuild({title, author, description, date: new Date().toString()}, content, passphrase, passphraseHint);
+};
+
+async function test() {
+    const passphrase = "my-secure-passphrase";
+
+    console.log("Testing...");
+    console.log("Passphrase: ", passphrase);
+
+    const hashedPassphrase = await hashPassphrase(passphrase);
+    const hexedHashedPassphrase = Array.from(hashedPassphrase).map(x => x.toString(16).padStart(2, '0')).join('');
+    console.log("Hash Passphrase: ", hashedPassphrase);
+    console.log("Hash Passphrase (Hex): ", hexedHashedPassphrase);
+
+    const salt = generateSalt();
+    console.log("Generated Salt: ", salt);
+    console.log("Generated Salt (Base64): ", bytesToBase64(salt));
+    console.log("Generated Salt (Hex): ", Array.from(salt).map(x => x.toString(16).padStart(2, '0')).join(''));
+
+    console.log("Generating locker key...");
+
+    {
+        const lockerKey = await generateKey(hashedPassphrase, salt);
+        console.log("Locker Key: ", lockerKey);
+        console.log("Locker Key (hex): ", Array.from(lockerKey).map(x => x.toString(16).padStart(2, '0')).join(''));
+    }
+
+    const lockerKey = await generateKey(hashedPassphrase, salt);
+    console.log("Locker Key: ", lockerKey);
+    console.log("Locker Key (hex): ", Array.from(lockerKey).map(x => x.toString(16).padStart(2, '0')).join(''));
+}
+test();
