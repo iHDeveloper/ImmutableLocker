b0568a31d54150636b5ea5dd9f987beb264d4582
iHDeveloper <git@ihdeveloper.me>
2021-09-17 18:36:22 +0300

    Implement tool to verify an immutable locker
    This tools helps the user verify an immutable locker using SHA-512
    and compare it with another SHA-512. The target SHA-512 can be
    read as HEX. It converts the HEX format to binary in order to compare
    it with another binary format. This is efficient and useful for verifying
    the existence of an immutable locker on a blockchain network, or through
    a message.

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,3 +1,9 @@
 export function bufferToHex(buffer) {
     return Array.from(buffer).map(x => x.toString(16).padStart(2, '0')).join('');
 }
+
+export function hexToBuffer(hex) {
+    return new Uint8Array(hex.match(/[\da-f]{2}/gi).map(function (h) {
+        return parseInt(h, 16)
+    }));
+}
